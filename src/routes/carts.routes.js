72aa4887ffc612375522af319f9ba6bdf1cd7f71
Santi Sanchez <santiagustinsanchez@gmail.com>
2024-05-21 14:32:00 -0300

    Subiendo mas archivos y productos funcionando en postman

---
+++ src/routes/carts.routes.js
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import cartManager from "../cartManager.js";
+import productManager from "../productManager.js";
+
+const router = Router();
+export default router;
