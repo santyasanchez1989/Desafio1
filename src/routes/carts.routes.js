582204eb6422c82f7d3e1ecf734c307f32708d4e
Santi Sanchez <santiagustinsanchez@gmail.com>
2024-05-21 19:11:08 -0300

    No logro hacer funcionar el Quantity dentro del CART.

--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -24,7 +24,7 @@ router.get("/:cid", async (req, res) => {
     res.status(200).json({ status: "success", cart });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ status: "Erro", msg: "Error interno del servidor" });
+    res.status(500).json({ status: "Error", msg: "Error interno del servidor" });
   }
 });
 
@@ -41,7 +41,7 @@ router.post("/:cid/product/:pid", async (req, res) => {
     res.status(200).json({ status: "success", cart });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ status: "Erro", msg: "Error interno del servidor" });
+    res.status(500).json({ status: "Error", msg: "Error interno del servidor" });
   }
 });
 
