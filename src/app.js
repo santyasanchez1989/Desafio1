99d988ca754779deb3dbb9e4581840d93d8f80fc
Santi Sanchez <santiagustinsanchez@gmail.com>
2024-06-04 22:59:13 -0300

    Subiendo datos importantes

--- src/app.js
+++ src/app.js
@@ -1,13 +1,31 @@
 import express from "express";
 import routes from "./routes/index.js";
+import __dirname from "./dirname.js";
+import handlebars from "express-handlebars";
+import { Server } from "socket.io";
+import viewsRoutes from "./routes/views.routes.js";
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.engine("handlebars", handlebars.engine()); 
+app.set("views", __dirname + "/views"); 
+app.set("view engine", "handlebars"); 
+app.use(express.static("public"));
 
 app.use("/api", routes);
 
-app.listen(8080, () => {
+app.use("/", viewsRoutes)
+
+
+const httpServer = app.listen(8080, () => {
   console.log("Servidor escuchando en el puerto 8080");
+});
+
+
+export const io = new Server(httpServer);
+
+io.on("connection", (socket) => {
+  console.log("Nuevo usuario Conectado");
 });
